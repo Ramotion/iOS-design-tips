d4c730c3eb4d216a1d2a6f2a564510da6ae9c9cb
Oleg Vasiliev <oleg.vasiliev@hotmail.com>
2014-10-17 11:54:29 +0400

    v.1.0 - First commit.

---
+++ js/Controller.js
@@ -0,0 +1,8 @@
+function Controller($scope) {
+    $scope.devices = devices
+    $scope.orientation = "port";
+    $scope.statbar = false;
+    $scope.video = true;
+}
+
+
---
+++ js/Devices.js
@@ -0,0 +1,58 @@
+//Devices map with ordering
+var devices = {
+    "1":{
+        "display_name":"iPhone 4",
+        "port_bar":"640x960",
+        "port":"640x920",
+        "land_bar":"960x640",
+        "land":"960x600",
+        "port_vid":"none",
+        "land_vid":"none"
+    },
+    "2":{
+        "display_name":"iPhone 5",
+        "port_bar":"640x1136",
+        "port":"640x1096",
+        "land_bar":"1136x640",
+        "land":"1136x600",
+        "port_vid":"640x1136",
+        "land_vid":"1136x640"
+    },
+    "3":{
+        "display_name":"iPhone 6",
+        "port_bar":"none",
+        "port":"750x1334",
+        "land_bar":"none",
+        "land":"1334x750",
+        "port_vid":"750x1334",
+        "land_vid":"1334x750"
+ },
+    "4":{
+        "display_name":"iPhone 6 Plus",
+        "port_bar":"none",
+        "port":"1242x2208",
+        "land_bar":"none",
+        "land":"2208x1242",
+        "port_vid":"1080x1920",
+        "land_vid":"1920x1080"
+},
+    "5":{
+        "display_name":"iPad",
+        "port_bar":"768x1024",
+        "port":"768x1004",
+        "land_bar":"1024x768",
+        "land":"1024x748",
+        "port_vid":"900x1200",
+        "land_vid":"1200x900"
+},
+    "6":{
+        "display_name":"iPad(hi-res)",
+        "port_bar":"1536x2048",
+        "port":"1536x2008",
+        "land_bar":"2048x1536",
+        "land":"2048x1496",
+        "port_vid":"900x1200",
+        "land_vid":"1200x900"
+}}
+
+
